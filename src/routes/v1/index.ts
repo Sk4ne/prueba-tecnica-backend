6a11e63fa0bfafca3811b335d2217f34822163d9
sk4ne <neider86@protonmail.com>
2025-03-25 23:51:11 -0500

    feat: Se agrega la referencia a los endpoints: products y orders

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,10 +1,14 @@
 import express, { Router } from 'express'
 
 import userRouter from './user'
+import productRouter from './product'
+import orderRouter from './order'
 
 const router:Router = express.Router()
 
 router.use(userRouter)
+router.use(productRouter)
+router.use(orderRouter)
 
 
 export default router
